c9ae93487ba764f1c576597966f6b56500be3860
Srabani-09 <srabanibehera1234@gmail.com>
2024-11-17 23:43:35 +0530

    changes

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,9 +4,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Notes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+    title = models.CharField(max_length=255)
+    description=models.TextField(null=True,blank=True)
+    
+    def __str__(self):
+        return self.title
     
     class Meta:
         verbose_name = "notes"
         verbose_name_plural = "notes"
+    
--- studentstudyportals/settings.py
+++ studentstudyportals/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dashboard',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'studentstudyportals.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -125,3 +125,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/"static"]
+CRISPY_TEMPLATE_PACK = "bootstrap"
